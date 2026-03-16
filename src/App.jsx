4bbe486fa71da1cade9bedbca4b04ec19fab7b33
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-16 09:27:01 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassles
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
