372280835e77959868115ffff110aded59f87c4c
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-17 15:02:04 +0900

    partial movie app
    add search functionality. will add input debouncing as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react'
+
+import MovieCard from './components/MovieCard.jsx'
+import Spinner from './components/Spinner.jsx'
 import Search from './components/Search.jsx'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
@@ -16,21 +19,41 @@ const API_OPTIONS = {
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
+    const [movieList, setMovieList] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
+        setIsLoading(true)
+        setErrorMessage('')
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            const endpoint = query
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
             const response = await fetch(endpoint, API_OPTIONS)
 
-            throw new Error('Failed to fetch movies')
+            if (!response) {
+                throw new Error('Failed to fetch movies')
+            }
+
+            const data = await response.json()
+
+            if (data.Response === 'False') {
+                setErrorMessage(data.Error || 'Failed to fetch movies')
+                setMovieList([])
+                return
+            }
+
+            setMovieList(data.results || [])
         } catch (error) {
             console.log(`Error fetching movies: ${error}`)
             setErrorMessage('Error fetching movies. Please try again later.')
+        } finally {
+            setIsLoading(false)
         }
     }
     useEffect(() => {
-        fetchMovies()
-    }, [])
+        fetchMovies(searchTerm)
+    }, [searchTerm])
 
     return (
         <main>
@@ -49,9 +72,17 @@ const App = () => {
                     />
                 </header>
                 <section className="all-movies">
-                    <h2>All Movies</h2>
-                    {errorMessage && (
+                    <h2 className="mt-[40px]">All Movies</h2>
+                    {isLoading ? (
+                        <Spinner />
+                    ) : errorMessage ? (
                         <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie} />
+                            ))}
+                        </ul>
                     )}
                 </section>
             </div>
