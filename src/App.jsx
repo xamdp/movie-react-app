55a040df4506897e35791ac1967637515afc33e0
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-17 09:39:40 +0900

    connect from app to tmdb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import Search from './components/Search.jsx'
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+    },
+}
 
 const App = () => {
-  return (
-    <main>
-      <div className="pattern" />
-      <div className="wrapper">
-        <header>
-          <h1>
-            Find <span className="text-gradient">Movies</span> You'll Enjoy
-            Without the Hassles
-          </h1>
-        </header>
-        <p>Search</p>
-      </div>
-    </main>
-  );
-};
+    const [searchTerm, setSearchTerm] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
 
-export default App;
+    const fetchMovies = async () => {
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            const response = await fetch(endpoint, API_OPTIONS)
+
+            throw new Error('Failed to fetch movies')
+        } catch (error) {
+            console.log(`Error fetching movies: ${error}`)
+            setErrorMessage('Error fetching movies. Please try again later.')
+        }
+    }
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
+    return (
+        <main>
+            <div className="pattern" />
+            <div className="wrapper">
+                <header>
+                    <img src="hero.png" alt="Hero Banner" />
+                    <h1>
+                        Find <span className="text-gradient">Movies</span>{' '}
+                        You'll Enjoy Without the Hassles
+                    </h1>
+
+                    <Search
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                    />
+                </header>
+                <section className="all-movies">
+                    <h2>All Movies</h2>
+                    {errorMessage && (
+                        <p className="text-red-500">{errorMessage}</p>
+                    )}
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default App
